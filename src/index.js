53edf392b463b3112779783bf41522290b136cc8
Yuliya <yulia.podd@gmail.com>
2020-10-25 19:45:57 +0200

    feat:add morse function

--- src/index.js
+++ src/index.js
@@ -37,8 +37,38 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const dot = '.';
+const dash = '-';
+let letter = '';
+
 function decode(expr) {
-    // write your solution here
+    for (let i = 0; i < expr.length; i+=10){
+        let j = i;
+          if (expr.charAt(i) == '*') {
+           letter += ' ';
+          continue;
+          }
+          while(expr.charAt(j) == '0'){
+          j++;
+          }
+                    
+          //here j face '1' 
+          let symbol = '';
+          for (let k = j; k < i+10; k+=2){
+              if (expr.charAt(k) == '1'&& expr.charAt(k+1) == '0') {
+                symbol += dot;
+              } else {
+                symbol += dash;
+              }
+          }
+          
+          //find in MORSE such symbol and set it to variable letter
+           letter += MORSE_TABLE[symbol];
+         
+          //make symbol be empty again
+          symbol = '';
+          
+        }
 }
 
 module.exports = {
